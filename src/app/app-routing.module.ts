b37c0de9fd36b52c6c33d5426603fa0f4f9a6bc4
cissoko97 <boriscissoko@gmail.com>
2022-03-06 22:45:58 +0100

    Fix issue ** on app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'task', loadChildren: () => import('./task/task.module').then(m => m.TaskModule) },
+  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
 
 @NgModule({
